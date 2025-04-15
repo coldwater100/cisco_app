07934a0873296c0c288c53cca151ec2738703533
lamb0693 <mar189@naver.com>
2025-04-15 16:27:05 +0900

    add alarm modified

--- lib/data/alarm_message.dart
+++ lib/data/alarm_message.dart
@@ -16,28 +16,15 @@ class AlarmMessage {
 
   factory AlarmMessage.fromMap(Map<String, dynamic> data) {
     final rawTimestamp = data['timestamp'];
-    DateTime parsedTimestamp;
-    print("<<<<rawTimeStamp in fromMap" + rawTimestamp.toString());
-    if (rawTimestamp != null && rawTimestamp is Timestamp) {
-      parsedTimestamp = rawTimestamp.toDate();
-    } else {
-      // 적절한 예외 처리 또는 로그 출력
-      print("rawTimestamp가 null이거나 Timestamp 타입이 아닙니다.");
-    }
+    String rawTimestampString = rawTimestamp.toString();
+    rawTimestampString = rawTimestampString.replaceAll(' UTC+9', '');
+    rawTimestampString = rawTimestampString.replaceFirst(RegExp(r'\d{4}년 '), '');
 
-    // if (rawTimestamp is Timestamp) {
-    //   parsedTimestamp = rawTimestamp.toDate();
-    // } else {
-    //   parsedTimestamp = rawTimestamp.toString() as DateTime;
-    // }
-    // print("<<<<parsedTimeStamp" + parsedTimestamp.toString());
 
     return AlarmMessage(
       mac: data['mac'] ?? '',
       alarm: data['alarm'] ?? '',
-      // timestamp: parsedTimestamp.toLocal(),
-      // formattedTime: DateFormat.Hms().format(parsedTimestamp.toLocal()),
-      formattedTime: rawTimestamp.toString());
+      formattedTime: rawTimestampString);
   }
 }
 
